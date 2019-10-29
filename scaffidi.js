cc060e0186c814463dd32bc9f6a91127cbf98c97
M.K. Irvine <irvinemk@gmail.com>
2019-10-28 21:46:12 -0400

    added mobile version

--- scaffidi.js
+++ scaffidi.js
@@ -1,1 +1,10 @@
+///not working!
+
+let navButton = document.querySelectorAll(".navButton")
+
+let changeColor = function() {
+    ocument.querySelectorAll(".navButton").style.color= "red"
+}
+
+document.querySelector(".navButton").addEventListener("mouseover", changeColor);
 
